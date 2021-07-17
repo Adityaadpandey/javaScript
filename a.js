798baea6f9df25c270b3a8c5432291830c90d5c9
aditya <adityapandeyadp@gmail.com>
2021-07-17 22:03:57 +0530

    commit on main

---
+++ a.js
@@ -0,0 +1,9 @@
+console.log("hi bro")
+// document.getElementsByTagName('h1')[0].style.background = "rgb(1, 183, 255)"
+// document.getElementsByid('click')[0].style.background = "rgb(1, 183, 255)"
+
+document.getElementsByTagName('h1')[0].style.background = "rgb(1, 183, 255)"
+document.getElementsByClassName('center2')[0].style.background = "rgb(1, 183, 255)"
+
+
+
